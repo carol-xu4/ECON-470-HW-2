03d258d135ef4b9dfb2cc668676cab930527afe0
Carol Xu <carol.xu@emory.edu>
2024-02-18 16:23:55 -0500

    PLOTSSSSAAA

--- analysis/Analysis Code.r
+++ analysis/Analysis Code.r
@@ -16,13 +16,15 @@ hospital_counts = final.hcris %>%
   count()
         #why are there 16 values in 2007 lol
 
-ggplot(hospital_counts, aes(x = fyear, y = n)) +
+hospital_counts_plot = ggplot(hospital_counts, aes(x = fyear, y = n)) +
   geom_line() +
   labs(
     x = "Year",
     y = "Number of Hospitals",
     title = "Number of Hospitals per Year") + 
     theme_minimal()
+ggsave("hospital_counts_plot.png")
+
 
 # 2 Unique hospital IDs (Medicare provider numbers)
 unique_hospital_ids = final.hcris %>%
@@ -32,20 +34,33 @@ print(unique_hospital_ids)
         # There are 48803 unique hospital IDs
 
 # 3 Distribution of total charges per year
-ggplot(final.hcris, aes(x = as.factor(fyear), y = tot_charges)) +
-  geom_violin(fill = "skyblue", color = "blue", alpha = 0.6) +  # Customize violin plot aesthetics
+charges_distribution = ggplot(final.hcris, aes(x = as.factor(fyear), y = tot_charges)) +
+  geom_violin(fill = "skyblue", color = "blue", alpha = 0.6) +
   labs(
     x = "Year",
     y = "Total Charges",
     title = "Distribution of Total Charges by Year"
   ) +
   theme_minimal()
+  ggsave("charges_distribution.png")
 
 # 4 Distribution of estimated prices by year
-discount_factor = 1-tot_discounts/tot_charges
-price_num = (ip_charges + icu_charges + ancillary_charges)*discount_factor - tot_mcare_payment
-price_denom = tot_discharges - mcare_discharges
-price = price_num/price_denom
+final.hcris = final.hcris %>%
+        mutate(discount_factor = 1-tot_discounts/tot_charges)
+final.hcris <- final.hcris %>%
+  mutate(price_num = (ip_charges + icu_charges + ancillary_charges) * discount_factor - tot_mcare_payment,
+    price_denom = tot_discharges - mcare_discharges,
+    price = price_num / price_denom)   
+
+estimated_prices_plot = ggplot(final.hcris, aes(x = as.factor(fyear), y = estimated_price)) +
+  geom_violin(fill = "skyblue", color = "blue", alpha = 0.6) +
+  labs(
+    x = "Year",
+    y = "Estimated Price",
+    title = "Distribution of Estimated Prices by Year"
+  ) +
+  theme_minimal() 
+print(estimated_prices_plot)
 
 # ATE ESTIMATES
 
